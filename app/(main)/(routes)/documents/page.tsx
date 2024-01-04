f30003bb18164d2136a4b5436f2dcc02c9da6c1b
AKACHI-4 <Adarshrawat8304@gmail.com>
2024-01-04 07:56:25 +0530

    Added main documents page

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,7 @@
+const Document = () => {
+  return (
+    <div>This is a protected page ! only authenticated users can access.</div>
+  );
+};
+
+export default Document;
---
+++ app/(main)/_components/navigation.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export const Navigation = () => {
+  return (
+    <>
+      <div>Hello from Navigation</div>
+    </>
+  );
+};
+
+export default Navigation;
