963fe06f92338acbf6512bb92b84360f0251332e
AKACHI-4 <Adarshrawat8304@gmail.com>
2024-01-19 21:56:08 +0530

    feat: add Document list

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -14,14 +14,14 @@ import { useMediaQuery } from "usehooks-ts";
 import { cn } from "@/lib/utils";
 import { UserItem } from "./useritem";
 import { Item } from "./item";
-import { useQuery, useMutation } from "convex/react";
+import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { toast } from "sonner";
+import { DocumentList } from "./document_list";
 
 export const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
-  const documents = useQuery(api.documents.get);
   const create = useMutation(api.documents.create);
 
   const isResizingRef = useRef(false);
@@ -146,9 +146,7 @@ export const Navigation = () => {
         </div>
 
         <div className="mt-4">
-          {documents?.map((document) => (
-            <p key={document._id}>{document.title}</p>
-          ))}
+          <DocumentList />
         </div>
 
         <div
